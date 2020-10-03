19dc835aa106f1fc8da7cce5e510da9ed7ca511e
gautammenon <gautam.menon01@gmail.com>
2020-10-03 16:28:09 +0530

    dev

---
+++ lib/Screens/EditorPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class EditorPage extends StatefulWidget {
+  @override
+  _EditorPageState createState() => _EditorPageState();
+}
+
+class _EditorPageState extends State<EditorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
